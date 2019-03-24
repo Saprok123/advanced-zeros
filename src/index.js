4ca8134fc62cbb711c5d78c786c2b8ce1a27c1cd
Saprok123 <schpack.maks@gmail.com>
2019-03-24 21:31:45 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let k = 1;
+  let sum = 0;
+  let mas=[];
+  let i = 2 ;
+  while (i <= base){
+    if (base % i == 0){
+      mas.push([i])
+      base=base / i;
+    }
+      else {i++};
+  };
+  while (i<=base){
+   if (base % i == 0){
+   mas.push([i])
+   base=base / i;
+  }
+   else {
+     i++};
+  };  
+  let x=Math.max(...mas) / 1;
+  let y=mas.filter(item => item ==x).length / 1
+  let a=mas.filter(item => item ==2).length / 1
+  while ((Math.floor(number/(Math.pow(x,k)))) >= 1) {
+    sum+=Math.floor(number/(Math.pow(x,k)));
+    k++;
+  };
+  return(Math.floor((sum/y)));
 }
